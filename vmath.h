8d0d0d98404658136075483383b36f22369afbc1
acatelan <anthonyjcatelani@gmail.com>
2021-07-19 15:33:55 -0700

    moved vec structs to top of vmath.h

--- vmath.h
+++ vmath.h
@@ -9,12 +9,6 @@ using namespace std;
 #ifndef VMATH_H
 #define VMATH_H
 
-void *vec3f_normalize(Vec3f v);
-void *vec3f_cross(Vec3f dst, Vec3f a, Vec3f b);
-void *vec3f_set(Vec3f dst, float x, float y, float z);
-void mtxf_align_terrain_normal(Mat4 dest, Vec3f upDir, Vec3f pos);
-void* linear_mtxf_mul_vec3f(Vec3f dst, Mat4 m, Vec3f v);
-
 typedef float Vec2f[2];
 typedef Surface Vec2S[2];
 typedef float Vec3f[3]; // X, Y, Z, where Y is up
@@ -24,5 +18,11 @@ typedef float Vec4f[4];
 typedef int16_t Vec4s[4];
 
 typedef float Mat4[4][4];
+
+void *vec3f_normalize(Vec3f v);
+void *vec3f_cross(Vec3f dst, Vec3f a, Vec3f b);
+void *vec3f_set(Vec3f dst, float x, float y, float z);
+void mtxf_align_terrain_normal(Mat4 dest, Vec3f upDir, Vec3f pos);
+void* linear_mtxf_mul_vec3f(Vec3f dst, Mat4 m, Vec3f v);
 
 #endif
